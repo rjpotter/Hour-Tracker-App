ce8292c661a98e32078697abbb13584faa7b1d96
rjpotter <rjpotter@uvm.edu>
2024-06-03 15:50:40 -0400

    Setting up Git Repo

---
+++ Hour Tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hour Tracker
+//
+//  Created by Ryan Potter on 6/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hour Tracker/Hour_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hour_TrackerApp.swift
+//  Hour Tracker
+//
+//  Created by Ryan Potter on 6/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Hour_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
